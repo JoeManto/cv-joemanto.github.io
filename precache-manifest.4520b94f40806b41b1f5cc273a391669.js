b8942fe12b1575fcba0287ed6f5da88c63d2a727
Joe Michael Manto <jfj5666@wade.wmich.edu>
2019-10-30 21:32:44 -0400

    Updates

---
+++ precache-manifest.4520b94f40806b41b1f5cc273a391669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a76744f6cc1dd92ada7d6e9711ca71d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccfe32db8f6050d4e40d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b2bb65aa83ee5853c1ee",
+    "url": "/static/js/2.a94e9ee1.chunk.js"
+  },
+  {
+    "revision": "ccfe32db8f6050d4e40d",
+    "url": "/static/js/main.e99cf5d5.chunk.js"
+  },
+  {
+    "revision": "90cd043654348d0e02f3",
+    "url": "/static/js/runtime-main.6c513046.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv-joemanto.github.io/precache-manifest.eb7406ede3ebffdd85086ba08d0b3f45.js"
+  "/precache-manifest.4520b94f40806b41b1f5cc273a391669.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cv-joemanto.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
