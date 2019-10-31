bb0c0a9a3e41e06b21e9a9ef8f0bf7497c223c9f
Joe Michael Manto <jfj5666@wade.wmich.edu>
2019-10-30 21:16:55 -0400

    Updates

---
+++ precache-manifest.eb7406ede3ebffdd85086ba08d0b3f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b267a3d2050e52429cdbe8a3ee4294e",
+    "url": "/cv-joemanto.github.io/index.html"
+  },
+  {
+    "revision": "bf52e9a504d212491152",
+    "url": "/cv-joemanto.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b2bb65aa83ee5853c1ee",
+    "url": "/cv-joemanto.github.io/static/js/2.a94e9ee1.chunk.js"
+  },
+  {
+    "revision": "bf52e9a504d212491152",
+    "url": "/cv-joemanto.github.io/static/js/main.4d01017f.chunk.js"
+  },
+  {
+    "revision": "090e35dbac217396fe47",
+    "url": "/cv-joemanto.github.io/static/js/runtime-main.ab2f9f5d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cv-joemanto.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
